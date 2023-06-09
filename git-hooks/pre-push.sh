b44d6433784c180d22c7c0328a2adf44242f0a7b
Alejandro Miranda <amiranda110500@gmail.com>
2023-06-09 20:11:33 +0100

    test

--- git-hooks/pre-push.sh
+++ git-hooks/pre-push.sh
@@ -5,7 +5,7 @@ current_branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 if [ $protected_branch = $current_branch ]
 then
-    read -p "You're about to push master, is that what you intended? [y|n] " -n 1 -r < /dev/tty
+    read -p "You're about to push master, is that what you intended 🥸? [y|n] " -n 1 -r < /dev/tty
     echo
     if echo $REPLY | grep -E '^[Yy]$' > /dev/null
     then
